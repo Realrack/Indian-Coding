dab67bfb12029e2caa3533e1a3e3a087bdc072e2
Realrack <ashishtanwar2191@gmail.com>
2022-12-12 10:01:49 +0530

    chsange

---
+++ src/components/courses-info-card.component.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+import { Card } from "react-native-paper";
+
+export default function CoursesInfoCard(courses = {}) {
+  const {} = courses;
+  return <Card></Card>;
+}
